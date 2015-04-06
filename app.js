2700232c6e76f8152de48236f9d2fb6edecb4030
michalurbanski <michal_urbanski@windowslive.com>
2015-04-06 23:22:31 +0200

    Adds first controller to html file

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 // javascript code goes here
 
+(function(){
+
+	// this requires ng-app="mainModule" directive
+	var app = angular.module("mainModule", []); // empty parentheses create new module, instead of searching one 
+
+
+	// in current version of angular controller needs to be in module
+	var MainCtrl = function($scope){
+		$scope.message = "Hello angular";
+	};
+
+
+	// required for module to work
+	// in addition parameters are passed as array in case of minification of these file
+	app.controller("MainCtrl", ["$scope", MainCtrl]);	
+}());
