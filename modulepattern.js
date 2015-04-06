79cae765de8916cb8ea2e052e8b06c27fa8df6e1
michalurbanski <michal_urbanski@windowslive.com>
2015-04-06 21:11:12 +0200

    Adds simple logging function for js

---
+++ modulepattern.js
@@ -0,0 +1,15 @@
+// module pattern sample
+
+// very simple logging function
+function log(message){
+	if(typeof console != "undefined"){
+		console.log(message);
+	}
+}
+
+
+// var person = function(){
+
+// };
+
+// person.speak
