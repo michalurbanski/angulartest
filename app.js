fe2087cde1ed05d5eebc3c06934ef5409538cf36
michalurbanski <michal_urbanski@windowslive.com>
2015-05-30 22:31:42 +0200

    Moves module creation to app.js file

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 (function(){
-	
+	// this requires ng-app="mainModule" directive to be present in html
+	var app = angular.module("mainModule", []); // empty parentheses create new module, instead of searching one 
+
+	// configuration function used for routes
+	app.config(function(){
+
+	});
 }());
